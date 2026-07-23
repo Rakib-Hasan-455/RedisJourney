a53ed4c2a9263c1565adbb12a523350d6aedadf0
Rakibul Hasan <hasanrakib455@gmail.com>
2026-07-24 01:41:50 +0600

    DS GetSet Weather

--- src/main/java/com/springredis/redisjourney/Controller/Lesson_1_DataStructures.java
+++ src/main/java/com/springredis/redisjourney/Controller/Lesson_1_DataStructures.java
@@ -1,4 +1,45 @@
 package com.springredis.redisjourney.Controller;
 
-public class Lession_1_DataStructures {
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+@RestController
+public class Lesson_1_DataStructures {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @PostConstruct
+    public void init() {
+        // Initialize the RedisTemplate
+        getSetWeather("Dhaka");
+    }
+
+    public void getSetRedis() {
+        redisTemplate.opsForValue().set("name1", "Rakib");
+        redisTemplate.opsForValue().set("name", "Rakib", 60L, TimeUnit.SECONDS);
+        System.out.println("Name from Redis: " + redisTemplate.opsForValue().get("name"));
+    }
+
+    // let's implement weather api with redis caching
+    public void getSetWeather(String city) {
+        // Check if the weather data is already in Redis
+        String weatherData = (String) redisTemplate.opsForValue().get("weather:" + city);
+        if (weatherData == null) {
+            // If not in Redis, fetch from the weather API and store in Redis
+            // This is a placeholder for the actual API call
+            weatherData = fetchWeatherFromAPI(city);
+            redisTemplate.opsForValue().set("weather:" + city, weatherData, 60L, TimeUnit.SECONDS); // Cache for 1 minutes
+        }
+        System.out.println(weatherData);
+    }
+
+    private String fetchWeatherFromAPI(String city) {
+        // This is a placeholder for the actual API call
+        return "Weather data for " + city + " time " + java.time.LocalDateTime.now();
+    }
 }
--- src/main/java/com/springredis/redisjourney/RedisJourneyApplication.java
+++ src/main/java/com/springredis/redisjourney/RedisJourneyApplication.java
@@ -1,13 +1,17 @@
 package com.springredis.redisjourney;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
-public class RedisJourneyApplication {
+public class RedisJourneyApplication{
 
-	public static void main(String[] args) {
-		SpringApplication.run(RedisJourneyApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisJourneyApplication.class, args);
+    }
 
 }
